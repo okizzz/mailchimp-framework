66a310b1f066a9f6316a092fb3a0acb3e83218ff
okizzz <47102435+okizzz@users.noreply.github.com>
2020-08-05 17:03:26 +0300

    conf .env

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import MailchimpMethods from "./mailchimp/MailchimpMethods.js";
 import MongooseMethods from "./mongo/MongooseMethods.js";
 import TelegramBotMethods from "./telegramBot/TelegramBotMethods.js";
 import startWriteLog from "./logging/StartWriteLog.js";
-process.env.TZ = "Europe/Moscow";
+const configEnv = dotenv.config({ path: process.argv[1].replace("index.js", ".env") });
 
 if (process.argv[2] == "--help") {
   console.dir({ Help: "--help", "Start passengers company": "--passengers" });
@@ -21,7 +21,7 @@ if (process.argv[2] == undefined || process.argv[2] != "--passengers") {
     MongooseMethods,
     TelegramBotMethods,
     async,
-    dotenv,
+    configEnv,
     startWriteLog,
   );
 }
