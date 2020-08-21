937f7ffad745d24b53b648afa4152850e41562f4
okizzz <shoose@protonmail.com>
2020-08-21 14:39:56 +0300

    fix index.js

--- index.js
+++ index.js
@@ -5,23 +5,29 @@ import MailchimpMethods from "./mailchimp/MailchimpMethods.js";
 import MongooseMethods from "./mongo/MongooseMethods.js";
 import TelegramBotMethods from "./telegramBot/TelegramBotMethods.js";
 import startWriteLog from "./logging/StartWriteLog.js";
-const configEnv = dotenv.config({ path: process.argv[1].replace("index.js", ".env") });
+const configEnv = dotenv.config({
+  path: process.argv[1].replace("index.js", ".env"),
+});
+const arg = process.argv[2];
 
-if (process.argv[2] == "--help") {
-  console.dir({ Help: "--help", "Start passengers company": "--passengers" });
-  process.exit(0);
-}
-
-if (process.argv[2] == undefined || process.argv[2] != "--passengers") {
-  console.log("wrong argument");
-  process.exit(100);
-} else {
-  startCompaignPassengers(
-    MailchimpMethods,
-    MongooseMethods,
-    TelegramBotMethods,
-    async,
-    configEnv,
-    startWriteLog,
-  );
+switch (arg) {
+  case "--help":
+    console.dir({
+      Help: "--help",
+      "Start passengers company": "--passengers",
+    });
+    break;
+  case "--passengers":
+    startCompaignPassengers(
+      MailchimpMethods,
+      MongooseMethods,
+      TelegramBotMethods,
+      async,
+      configEnv,
+      startWriteLog,
+    );
+    break;
+  default:
+    console.log("wrong argument");
+    process.exit(100);
 }
